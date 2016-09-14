18a413ecf8a2a4511e1c148eb9a5bcb563f6e143
Cavin <xjr30226@126.com>
2016-09-14 13:03:38 +0800

    use template

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask import render_template
 from flask import request
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
