fc3deb40c418b28065026098899c1f715203550a
Cavin <xjr30226@126.com>
2016-09-15 12:16:32 +0800

    添加moment.js

--- hello.py
+++ hello.py
@@ -4,13 +4,16 @@ from flask import request
 from flask import url_for
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
