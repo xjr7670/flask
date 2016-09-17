5007ff0a14b2203632e036d542215a473e885c40
Cavin <xjr30226@126.com>
2016-09-17 14:24:45 +0800

    使用WTforms表单

--- hello.py
+++ hello.py
@@ -2,18 +2,37 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import session
+from flask import redirect
+from flask import flash
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Required 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'xjr7670'
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
