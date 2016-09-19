de29821c5a27cef37754f16c2bde83c27adc5d57
root <root@localhost.localdomain>
2016-09-19 11:44:23 +0800

    使用sqlite数据库

--- hello.py
+++ hello.py
@@ -1,23 +1,40 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import url_for
-from flask import session
-from flask import redirect
-from flask import flash
+#-*- coding: utf-8 -*-
+
+import os
+from flask import Flask, render_template, request, url_for, session, redirect, flash
 from flask_bootstrap import Bootstrap
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_moment import Moment
 from flask_wtf import Form
-from wtforms import StringField
-from wtforms import SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+# 配置web表单防跨站攻击密钥
+app.config['SECRET_KEY'] = 'xjr7670'
+
+# 配置数据库路径
+# 本例使用的是sqlite
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+# 把SQLCHEMY_COMMIT_ON_TEARDOWN设置为True可以在每次请求结束后自动提交
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
-app.config['SECRET_KEY'] = 'xjr7670'
+# 为shell命令添加一个上下文
+# 这样就不用在每次启动shell时都要导入数据库实例和模型
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -27,12 +44,17 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -45,6 +67,27 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+# 定义Role和User模型
+# 也就是定义这两个表
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 if __name__ == '__main__':
     manager.run()
